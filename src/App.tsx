059c7296e2f6762f395fbd2f96583400e2938677
tyoung <taeyoung917@gmail.com>
2023-01-22 22:33:24 +0900

    💄  mapAPI , MainUI add @young-02 main API #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { Router } from "./Router";
+import React from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { Reset } from 'styled-reset';
+import { Router } from './Router';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Reset />
       <Router />
     </QueryClientProvider>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+import { IntroItem } from './main/IntroItem';
+import { Mapsprac } from './main/Mapsprac';
+import { Nav } from './Nav';
+
 export const Home = () => {
-  return <h1>홈</h1>;
+  return (
+    <MainContent>
+      <IntroItem />
+      <Mapsprac />
+    </MainContent>
+  );
 };
+
+const MainContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: calc(100% - 160px);
+`;
