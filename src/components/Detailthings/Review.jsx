21f35c1f6fc13c36f032bbc75954eda8b5d6bcca
Jaeyoung <dlawodud96@naver.com>
2023-01-26 17:46:47 +0900

    Revert "✅  AddReview 버튼 클릭시 파이어베이스 저장 #8 #9 #10 #11"
    This reverts commit 71ad7ae46e5a78c352b17daa46a269c52c76156d.

--- src/components/Detailthings/Review.jsx
+++ src/components/Detailthings/Review.jsx
@@ -24,6 +24,25 @@ function Review() {
   const { isLoading: createLoading, mutate: createMutate } =
     useMutation(addReview);
 
+  // cafeId와 일치하는 review들을 화면에 띄움
+  const currentUid = "임재영"; // 임시값 추후 수정 예정
+
+  const q = query(
+    collection(dbService, "review"),
+    where("uid", "==", currentUid)
+  );
+
+  const getMyReviewList = async () => {
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      setMyReviews((prev) => [...prev, doc.data()]);
+    });
+  };
+
+  useEffect(() => {
+    getMyReviewList();
+  }, []);
+
   // 버튼 클릭시 revieData를 가져오는 함수
   const addCreateReview = () => {
     const reviewData = {
@@ -80,11 +99,11 @@ function Review() {
         {/* 모든 리뷰에 관한 것들  */}
 
         {/* 리뷰등록 Btn 클릭시 생기는 생성 컴포넌트 */}
-        {/* <AddReview
+        <AddReview
           reviewData={reviewData}
           setReviews={setReviews}
           reviews={reviews}
-        /> */}
+        />
 
         {/* ReviewItem들 : 등록되어있는 리뷰들 */}
         <ReviewItem reviewData={reviewData}></ReviewItem>
