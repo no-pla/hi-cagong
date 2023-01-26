f6e2017620aa1b4bed511274aad037ae29a9a5bb
tyoung <taeyoung917@gmail.com>
2023-01-26 11:13:18 +0900

    💄 merge push @young-02 main API #1

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { DetailContent } from "./DetailContent";
-import Review from "./Detailthings/Review";
+import { useState } from 'react';
+import { DetailContent } from './DetailContent';
+import Review from './Detailthings/Review';
 
 export const Detail = () => {
   const [review, setReview] = useState();
 
   return (
-    <>
+    <div>
       <DetailContent />
       <Review review={review} setReview={setReview} />
-    </>
+    </div>
   );
 };
