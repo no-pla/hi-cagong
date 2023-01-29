87e4104e64776c9e45fdae10acd7244002f3c43c
tyoung <taeyoung917@gmail.com>
2023-01-29 15:45:43 +0900

    ✅  npm install @young-02

--- src/components/atom.ts
+++ src/components/atom.ts
@@ -1,16 +1,16 @@
-import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
 
 export const searchStoreData = atom({
-  key: "storeData",
-  default: "스타벅스 강남",
+  key: 'storeData',
+  default: '스타벅스 강남',
   effects_UNSTABLE: [persistAtom],
 });
 
 export const currentUserUid = atom({
-  key: "currentUserUid",
+  key: 'currentUserUid',
   default: null,
   effects_UNSTABLE: [persistAtom],
 });
